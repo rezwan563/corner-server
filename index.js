fbd8d800e43c6dabbe31aa84a1ed85e1f6612385
Rezwan <t.rewan17@gmail.com>
2023-07-20 12:03:08 +0600

    Backend for corner app

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
+const port = process.env.PORT || 5000;
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri).then(() => console.log("connected to mongodb"));
+
+// middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('common'));
+
+app.use('/corner_app/users', userRouter)
+app.use('/corner_app/auth', authRouter)
+
+
+
+app.listen(port, () => {
+  console.log("Server is running");
+});
---
+++ routes/users.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+// update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("you can only update your account");
+  }
+});
+// delete user
+// get a user
+// follow a user
+// unfollow a user
+
+module.exports = router;
